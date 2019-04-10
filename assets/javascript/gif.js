b8a837b2f2e1e31dc50aa97afda9380e3b0ba3ee
kyang0626 <46052112+kyang0626@users.noreply.github.com>
2019-04-10 10:11:25 -0400

    updated

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -58,7 +58,7 @@ $("button").on("click", function() {
                     $(this).attr("src", $(this).attr("data-animate"));
                     $(this).attr("data-state", "animate");
                    } else {
-                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("src", $(this).attr("data-still"));
                     $(this).attr("data-state", "still");
                   }
             })
